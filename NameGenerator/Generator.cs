6e57781bedd5ed662efc9290d5dd72d433d3039b
Vibhor Singh <vibdaddy12@gmail.com>
2021-03-27 10:11:34 +0100

    first commit

---
+++ NameGenerator/Generator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static NameGenerator.Name;
+
+namespace NameGenerator
+{
+    public class Generator
+    {
+        private Random random = new Random();
+        private string[] GamerTags;
+        private string[] Realistic;
+        public NameTypes GeneratorFlags = NameTypes.None;
+
+
+        public string Generate()
+        {
+
+            switch (random.Next(1, 3))
+            {
+                case 1:
+                    return "Butt";
+                case 2:
+                    return "Yeaz";
+                case 3:
+                    return "T";
+                default:
+                    return "Bolb";
+            }
+        }
+
+
+
+
+    }
+}
---
+++ NameGenerator/Name.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NameGenerator
+{
+    public static class Name
+    {
+        [Flags]
+        public enum NameTypes : long
+        {
+            None = 0,
+            GamerTags = 1,
+            Realistic = 1 << 1,
+            Gibberish = 1 << 2,
+            Numbers = 1 << 3,
+            SpecialCharacters = 1 << 4,
+            OtherLanguages = 1 << 5
+        }
+
+
+
+    }
+}
